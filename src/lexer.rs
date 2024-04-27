932612a4649ec756fdb77f3f8c5216cc6c75986c
wysiwys <144733119+wysiwys@users.noreply.github.com>
2024-04-28 01:33:10 +0800

    add license

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,6 @@
+// based loosely on scanner.c
 /*
+ * Copyright © 2012 Ran Benita <ran234@gmail.com>
  * Copyright © 2024 wysiwys
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -19,7 +21,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- *
  */
 
 pub(crate) use crate::lexer_utils::*;
