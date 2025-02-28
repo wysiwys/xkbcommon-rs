cef32b538d923fcfba8bd0edf80b9d9d7a9021ba
wysiwys <144733119+wysiwys@users.noreply.github.com>
2025-02-28 18:42:54 +0100

    replace references to libxkbcommon 1.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,7 @@
  */
 
 //!
-//! A port of `libxkbcommon` version `1.7.0` in safe Rust.
+//! A port of `libxkbcommon` version `1.8.0` in safe Rust.
 //!
 //! ### Use in Wayland client
 //! This crate is intended for use within a Wayland client written in Rust. It provides `Send + Sync` implementations of [Keymap] and [State].
